5816f8a6a97937e4881ee0e87eb2a78d32ed16f0
DeveloperXPK <andresgiraldo05a@gmail.com>
2025-01-30 12:24:22 -0500

    Agregar animaciones de aparición y deslizamiento en elementos, y mejorar estilos en CSS para una mejor experiencia visual

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('appear');
+                entry.target.classList.remove('disappear');
+            } else {
+                entry.target.classList.remove('appear');
+                entry.target.classList.add('disappear');
             }
         });
     });
 
     document.querySelectorAll('.fade-in-up').forEach(element => {
+        observer.observe(element);
+    });
+
+    document.querySelectorAll('.slideInRight').forEach(element => {
         observer.observe(element);
     });
 });
