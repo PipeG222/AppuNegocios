35698e829e853c22e921bbd3b2ad50600f2a6470
DeveloperXPK <andresgiraldo05a@gmail.com>
2025-02-18 01:15:06 -0500

    Agregar estilos responsivos y un video de demostración en la página de checkout, además de limpiar el código HTML.
    Modificacion de diseno en planes Appu

--- script.js
+++ script.js
@@ -19,49 +19,27 @@ document.addEventListener('DOMContentLoaded', function () {
         observer.observe(element);
     });
 
-    if (window.location.pathname === '/index.html' || window.location.pathname === '/') {
-        document.getElementById("formularioRegistro").addEventListener("submit", async (event) => {
-            event.preventDefault();
+    document.getElementById("formularioRegistro").addEventListener("submit", async (event) => {
+        event.preventDefault();
 
-            const datos = {
-                nombre: document.getElementById("nombre2").value,
-                email: document.getElementById("email2").value,
-                telefono: document.getElementById("telefono2").value,
-            };
+        const datos = {
+            nombre: document.getElementById("nombre2").value,
+            email: document.getElementById("email2").value,
+            telefono: document.getElementById("telefono2").value,
+        };
 
-            console.log("Datos a enviar:", datos);
-            const response = await fetch("https://34053pzrhb.execute-api.us-east-1.amazonaws.com/produccion/registro", {
-                method: "POST",
-                body: JSON.stringify(datos),
-                headers: { "Content-Type": "application/json" }
-            });
-
-            console.log("Respuesta del servidor:", response);
-            alert("¡Gracias por registrarte! Pronto nos pondremos en contacto contigo.");
-            document.getElementById("nombre2").value = "";
-            document.getElementById("email2").value = "";
-            document.getElementById("telefono2").value = "";
+        console.log("Datos a enviar:", datos);
+        const response = await fetch("https://34053pzrhb.execute-api.us-east-1.amazonaws.com/produccion/registro", {
+            method: "POST",
+            body: JSON.stringify(datos),
+            headers: { "Content-Type": "application/json" }
         });
 
-    }
-
-    const imageContainers = document.querySelectorAll('.image-container');
-
-    imageContainers.forEach(container => {
-        const images = container.querySelectorAll('.sequence-image');
-        let currentIndex = 0;
-
-        function showNextImage() {
-            images[currentIndex].classList.remove('active');
-            currentIndex = (currentIndex + 1) % images.length;
-            images[currentIndex].classList.add('active');
-        }
-
-        // Mostrar la primera imagen inicialmente
-        images[currentIndex].classList.add('active');
-
-        // Cambiar la imagen cada 3 segundos
-        setInterval(showNextImage, 800);
+        console.log("Respuesta del servidor:", response);
+        alert("¡Gracias por registrarte! Pronto nos pondremos en contacto contigo.");
+        document.getElementById("nombre2").value = "";
+        document.getElementById("email2").value = "";
+        document.getElementById("telefono2").value = "";
     });
 
     const hamburger = document.getElementById("hamburger");
